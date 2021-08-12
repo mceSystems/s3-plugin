2c9aeaeb58917dca9227aac5ad0f0f6a85f3e09a
jonkipu <jonkipu>
2021-08-12 15:21:55 +0300

    generate link on page load instead upon clicking the artifact

--- src/main/java/hudson/plugins/s3/FingerprintRecord.java
+++ src/main/java/hudson/plugins/s3/FingerprintRecord.java
@@ -6,6 +6,28 @@ import hudson.model.Run;
 import jenkins.model.Jenkins;
 import org.kohsuke.stapler.export.Exported;
 import org.kohsuke.stapler.export.ExportedBean;
+import com.amazonaws.services.s3.AmazonS3Client;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.ResponseHeaderOverrides;
+import hudson.Functions;
+import jenkins.model.RunAction2;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+
+import hudson.model.Run;
+import org.kohsuke.stapler.export.Exported;
+import org.kohsuke.stapler.export.ExportedBean;
 
 import java.io.IOException;
 import java.io.Serializable;
@@ -18,14 +40,17 @@ public class FingerprintRecord implements Serializable {
     private final S3Artifact artifact;
     private boolean keepForever;
     private boolean showDirectlyInBrowser;
+    private S3Profile profile;
+    private Run<?, ?> run;
 
-
-    public FingerprintRecord(boolean produced, String bucket, String name, String region, String md5sum) {
+    public FingerprintRecord(boolean produced, String bucket, String name, String region, String md5sum, Run<?, ?> run, S3Profile profile) {
         this.produced = produced;
         this.artifact = new S3Artifact(region, bucket, name);
         this.md5sum = md5sum;
         this.showDirectlyInBrowser = false;
         this.keepForever = false;
+        this.run = run;
+        this.profile = profile;
     }
 
     Fingerprint addRecord(Run<?, ?> run) throws IOException {
@@ -56,8 +81,11 @@ public class FingerprintRecord implements Serializable {
 
     @Exported
     public String getLink() {
-        //Chrome and IE convert backslash in the URL into forward slashes, need escape with %5c
-        return artifact.getName().replace("\\","%5C");
+        final S3Profile s3 = this.profile;
+        final AmazonS3Client client = s3.getClient(this.getArtifact().getRegion());
+        final String url = getDownloadURL(client, s3.getSignedUrlExpirySeconds(), run, this);
+
+        return url;
     }
 
     @Exported
@@ -68,5 +96,22 @@ public class FingerprintRecord implements Serializable {
     @Exported
     public S3Artifact getArtifact() {
         return artifact;
+    }
+
+    private String getDownloadURL(AmazonS3Client client, int signedUrlExpirySeconds, Run run, FingerprintRecord record) {
+        final Destination dest = Destination.newFromRun(run, record.getArtifact());
+        final GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(dest.bucketName, dest.objectName);
+        request.setExpiration(new Date(System.currentTimeMillis() + signedUrlExpirySeconds*1000));
+
+        if (!record.isShowDirectlyInBrowser()) {
+            // let the browser use the last part of the name, not the full path
+            // when saving.
+            final ResponseHeaderOverrides headers = new ResponseHeaderOverrides();
+            final String fileName = (new File(dest.objectName)).getName().trim();
+            headers.setContentDisposition("attachment; filename=\"" + fileName + '"');
+            request.setResponseHeaders(headers);
+        }
+
+        return client.generatePresignedUrl(request).toExternalForm();
     }
 }
